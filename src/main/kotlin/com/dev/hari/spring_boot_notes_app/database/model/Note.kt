017cc66a578fdcda3b34030cc346537789ba2abc
Hari <49642836+kris175@users.noreply.github.com>
2025-06-08 17:12:46 +1000

    Added note data class

---
+++ src/main/kotlin/com/dev/hari/spring_boot_notes_app/database/model/Note.kt
@@ -0,0 +1,15 @@
+package com.dev.hari.spring_boot_notes_app.database.model
+
+import org.bson.types.ObjectId
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
+import java.time.Instant
+
+@Document("notes")
+data class Note(
+    val title: String,
+    val content: String,
+    val color: Long,
+    val createdAt: Instant = Instant.now(),
+    @Id val id: ObjectId = ObjectId.get()
+)
