aa2ff9595a9d55ad6d35e7593ce12790d65d6458
Hari <49642836+kris175@users.noreply.github.com>
2025-06-09 11:41:17 +1000

    Added jwt service methods

---
+++ src/main/kotlin/com/dev/hari/spring_boot_notes_app/security/JwtService.kt
@@ -0,0 +1,58 @@
+package com.dev.hari.spring_boot_notes_app.security
+
+import io.jsonwebtoken.Claims
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.security.Keys
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Service
+import java.util.Base64
+import java.util.Date
+
+@Service
+class JwtService (
+    @Value("JWT_SECRET") private val jwtSecret: String
+) {
+
+    private val secretKey = Keys.hmacShaKeyFor(Base64.getDecoder().decode(jwtSecret))
+    private val accessTokenValidityMs = 15L * 60 * 1000L // 15 minutes
+    private val refreshTokenValidityMs = 30L * 24 * 60 * 60 * 1000L // 30 days
+
+    private fun generateToken(
+        userId: String,
+        type: String,
+        expiry: Long
+    ) : String {
+        val now = Date()
+        val expiryDate = Date(now.time + expiry)
+        return Jwts.builder()
+            .subject(userId)
+            .claim("type", type)
+            .issuedAt(now)
+            .expiration(expiryDate)
+            .signWith(secretKey, Jwts.SIG.HS256)
+            .compact()
+    }
+
+    fun generateAccessToken(userId: String): String {
+        return generateToken(userId, "access", accessTokenValidityMs)
+    }
+    fun generateRefreshToken(userId: String): String {
+        return generateToken(userId, "refresh", refreshTokenValidityMs)
+    }
+
+//    fun validateAccessToken(token: String) : Boolean {
+//
+//    }
+
+    private fun praseAllClaims(token: String): Claims? {
+        return try {
+            Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .payload
+        } catch (e: Exception) {
+            null
+        }
+    }
+}
